de65040de265fdb45f42edd82dc8a77f077e4b57
Zannatul Haque <ferdoussiam@gmail.com>
2016-02-01 17:26:58 +0600

    Dynamic <thead>,<tbody>,<tfoot>
    Passing parameter and looping

--- library.php
+++ library.php
@@ -1,5 +1,40 @@
+
+
 <?php
 
 class DynamicTableGenerator {
-    
+
+    public function generateTable($numberOfRow, $argumentArray) {
+        $tableString = '';
+        $tableString .='<table border="1">';
+        $tableString .='<thead>';
+        $tableString .='<tr>';
+        foreach ($argumentArray as $value) {
+            $tableString .='<td>' . $value . '</td>';
+        }
+        $tableString .='</tr>';
+        $tableString .='<thead>';
+        $tableString .='<tbody>';
+        for ($i = 0; $i < $numberOfRow; $i++) {
+            $tableString .='<tr>';
+            $tableString .='<td>Sim</td>';
+            $tableString .='<td>Sim</td>';
+            $tableString .='<td>Sim</td>';
+            $tableString .='</tr>';
+        }
+        $tableString .='</tbody>';
+        $tableString .='<tfoot>';
+        $tableString .='<tr>';
+        foreach ($argumentArray as $value) {
+            $tableString .='<td>' . $value . '</td>';
+        }
+        $tableString .='</tr>';
+        $tableString .='<tfoot>';
+        $tableString .='</table>';
+        return $tableString;
+    }
+
 }
+
+$obj = new DynamicTableGenerator();
+echo $obj->generateTable(10, array('Name', 'Age', 'Date Of Birth'));
